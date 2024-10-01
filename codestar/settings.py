17c2573c8497303b5a5656b05f197d141ee4184b
Monika Mak <monika1986mak@gmail.com>
2024-10-01 14:07:33 +0000

    blog field settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y2q5-l!+e#)xbhq=waj%q4z8)))6q&!h^5yr_swgi+v^6^vx#j'
 SECRET_KEY= os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 DEBUG = False
 
 ALLOWED_HOSTS = ['8000-monikamak-djangoblog-3fa05v77bog.ws.codeinstitute-ide.net','.herokuapp.com']
