9876b2dda4ffb70c768be32c306495a11f2be66d
Monika Mak <monika1986mak@gmail.com>
2024-10-08 12:54:10 +0000

    models file update

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,14 @@ class Post(models.Model):
     exerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+    Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="commenter"
+)
+    body = models.TextField()
+    approved = BooleanField(default=False)    
+    created_on = models.DateTimeField(auto_now_add=True)
